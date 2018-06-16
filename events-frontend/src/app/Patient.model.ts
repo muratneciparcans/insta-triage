899a5f2ed3ce80673ca916c43a6b322569ba1fe0
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2018-06-16 10:37:55 +0200

    sending data to UI

---
+++ events-frontend/src/app/Patient.model.ts
@@ -0,0 +1,10 @@
+export class PatientModel {
+  public name: string;
+  public start_date: Date;
+  public end_date: Date;
+  public action: string;
+  public notified: string;
+  public estimated_waiting: string;
+  public heartrate: string;
+  constructor() {}
+}
--- events-frontend/src/app/app.component.ts
+++ events-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WebsocketService } from './websocket.service';
+import {PatientModel} from './Patient.model'
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,22 @@ import { WebsocketService } from './websocket.service';
 })
 export class AppComponent {
   public payloads: any = [];
+  public patient: PatientModel = new PatientModel();
 
   public estimated_waiting_time: string;
 
   constructor(private ws: WebsocketService) {}
 
   ngOnInit() {
-    this.ws.on('registry', (data) => {
+    this.ws.on('data', (data) => {
       console.log(data);
       // do stuff
-      this.payloads.push(data.payload);
+      this.payloads.push(data);
+      for (let k of Object.getOwnPropertyNames(data)) {
+        console.log(k, ' :',  data[k])
+        this.patient[k] = data[k];
+      }
+      console.log(this.patient);
     });
   }
 }
