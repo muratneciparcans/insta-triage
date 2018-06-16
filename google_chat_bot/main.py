0e8fccf906b175d1d6c68579d707b36a54a9f2a3
Maximilian Schulz <max@roboratory.com>
2018-06-16 03:46:48 +0200

    Delete unused application

--- google_chat_bot/main.py
+++ google_chat_bot/main.py
@@ -4,20 +4,8 @@ from __future__ import division
 
 from src.script_parser import start_script
 
-
 def main():
     start_script()
-
-
-    # Let's see, maybe we want this
-    # conf = Configuration("./mapper.txt")
-
-    # print('Standby mode...\nPress any key to record')
-
-    # app = Application(False)
-    # while True:
-    #     app.listen_key()
-
 
 if __name__ == '__main__':
     main()
